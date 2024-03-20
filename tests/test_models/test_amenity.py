8a371c55bccd689eccfbcdd6c81e6f952ac27d1d
Jesufemi <oladapofemmy@gmail.com>
2024-03-20 17:11:24 +0100

    add test for db_storage

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-""" """
+"""a module that tests Amenity class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
+import MySQLdb
+from os import getenv
+
+# host = getenv('HBNB_MYSQL_HOST')
+# user = getenv('HBNB_MYSQL_USER')
+# password = getenv('HBNB_MYSQL_PWD')
+# mydb = getenv('HBNB_MYSQL_DB')
+
+# db = MySQLdb.connect(host='localhost', user=user, password=password, db=mydb)
+# cur = db.cursor()
+
 
 class test_Amenity(test_basemodel):
-    """ """
+    """a class that tests amentiy"""
 
     def __init__(self, *args, **kwargs):
         """ """
