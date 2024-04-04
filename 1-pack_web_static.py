d956afdfe947cbf00f0907b6a32815cbf8150a41
Jesufemi <oladapofemmy@gmail.com>
2024-04-04 20:54:17 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,10 +2,15 @@
 """a fabfile that creates archive"""
 from fabric.api import *
 import time
+
+
 def do_pack():
     """the function to create the archive"""
     local("mkdir /versions")
-    name_of_archive = "web_static_{}.tgz".format(time.strftime(%Y%m%d%H%M%S,
-                                             time.localtime))
-    local(f"tar -cvzf versions/{name_of_archive} web_static")
-    return name_of_archive
+    name_of_archive = "web_static_{}.tgz".format(time.strftime("%Y%m%d%H%M%S",
+                                                 time.localtime))
+    create = local(f"tar -cvzf versions/{name_of_archive} web_static")
+    if create is not None:
+        return name_of_archive
+    else:
+        return None
