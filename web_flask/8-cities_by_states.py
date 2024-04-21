75bdb7555c7bcf9e25a14f918100eb0f7a91dc17
Jesufemi <oladapofemmy@gmail.com>
2024-04-21 23:17:16 +0100

    sort8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_state():
     """definition for /cities_by_state"""
-    all_states = storage.all(State)
-    states = all_states.values()
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
