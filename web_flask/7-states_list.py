dedd5e028e0bb5d50350e75df9ab2b5fff064ef7
Jesufemi <oladapofemmy@gmail.com>
2024-04-21 23:19:30 +0100

    sort7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     '''definition for /states_list'''
-    all_states = storage.all(State)
-    states = all_states.values()
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
