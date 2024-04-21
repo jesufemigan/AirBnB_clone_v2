53e9f58f804e6d04336bb806edc12bafdaa2129e
Jesufemi <oladapofemmy@gmail.com>
2024-04-21 22:58:43 +0100

    f

--- models/place.py
+++ models/place.py
@@ -17,25 +17,24 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if models.storage_type == 'db':
-        __tablename__ = "places"
-        name = Column(String(128), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        description = Column(String(1024))
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete")
-        amenities = relationship(
-            "Amenity", secondary="place_amenity", viewonly=False)
-    else:
-        amenity_ids = []
-
+    __tablename__ = "places"
+    name = Column(String(128), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    reviews = relationship("Review", backref="place",
+                            cascade="all, delete")
+    amenities = relationship(
+        "Amenity", secondary="place_amenity", viewonly=False)
+    amenity_ids = []
+    
+    if models.storage_type != "db":
         @property
         def reviews(self):
             """getter attribute for reviews"""
